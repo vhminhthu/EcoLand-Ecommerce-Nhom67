9386c8abed81a505cb4ba070e6742cef0a0d95c1
PhamTien-363296 <phamnguyenthuytien3007@gmail.com>
2025-02-26 02:44:36 +0700

    category management

--- backend/routes/danhmuc.route.js
+++ backend/routes/danhmuc.route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {addDanhMuc, getDanhMuc} from '../controllers/danhmuc.controller.js'
+import {addDanhMuc, getDanhMuc, suaDanhMuc} from '../controllers/danhmuc.controller.js'
 
 const router = express.Router()
 
 router.post("/them", addDanhMuc)
 router.get("/lay", getDanhMuc)
+router.patch("/sua/:id", suaDanhMuc)
 
 export default router
