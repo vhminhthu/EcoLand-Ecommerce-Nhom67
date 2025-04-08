1b66d6b200ca7c54cdfd99a36eab2ec7c05104bf
PhamTien-363296 <phamnguyenthuytien3007@gmail.com>
2025-04-09 04:30:51 +0700

    fix code

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -12,46 +12,34 @@ passport.use(new GoogleStrategy(
     callbackURL: "/api/auth/google/callback",
     passReqToCallback: true,
   },
+
   async (req, accessToken, refreshToken, profile, done) => {
     try {
-      const email = profile.emails[0].value;
-      let user = await Nguoidung.findOne({ email });
+      let user = await Nguoidung.findOne({ googleId: profile.id });
 
-     
-      if (user && user.matKhau) {
-        return done(null, false, { message: "Email này đã đăng ký bằng mật khẩu" });
-      }
-
-     
       if (!user) {
-        user = await Nguoidung.create({
-          googleId: profile.id,
-          tenNguoiDung: profile.displayName,
-          email,
-          anhND: profile.photos[0].value,
-        });
+        // check xem có người dùng nào đã đăng ký bằng email và có mật khẩu chưa
+        user = await Nguoidung.findOne({ email: profile.emails[0].value });
+      
+        if (user && user.matKhau) {
+          return done(null, false, { message: "Email này đã đăng ký bằng mật khẩu" });
+        }
+      
+        if (!user) {
+          user = await Nguoidung.create({
+            googleId: profile.id,
+            tenNguoiDung: profile.displayName,
+            email: profile.emails[0].value,
+            anhND: profile.photos[0].value,
+          });
+        }
       }
-
       return done(null, user);
+      
     } catch (error) {
       return done(error, null);
     }
   }
 ));
-
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await Nguoidung.findById(id);
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-});
 
 export default passport;
--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,6 @@ cloudinary.config({
 
 const PORT = process.env.PORT || 5000;
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 app.use(passport.initialize());
 
